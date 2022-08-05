9f67e72ccc5707182166c804bd765d780096189b
Notch-Man <samad.narayan@outlook.com>
2022-08-05 16:49:53 +0530

    Some commit

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script>
+  import { onMount } from "svelte";
+  import "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js";
+  import QuoteCard from "./lib/QuoteCard.svelte";
+  import axios from "axios";
+  import gradient from "random-gradient";
+
+  let quote;
+  let isLoading = false;
+  let endpoint = "https://api.quotable.io/random";
+
+  async function getQuote() {
+    isLoading = true;
+    const res = await axios.get(endpoint);
+    quote = res.data;
+    isLoading = false;
+  }
+
+  onMount(() => {
+    getQuote();
+  });
+  $: console.log(quote);
+</script>
+
+<main>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+    crossorigin="anonymous"
+  />
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+  />
+
+  <div class="content bg">
+    <QuoteCard quote={quote?.content} author={quote?.author} />
+
+    <button
+      class="btn btn-primary"
+      disabled={isLoading}
+      on:click={() => {
+        getQuote();
+      }}>Regenerate <i class="bi bi-arrow-clockwise" /></button
+    >
+  </div>
+</main>
+
+<style>
+  .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* margin-top: 100px; */
+    width: 100%;
+    height: 100vh;
+    transition: 0.5s ease-in-out;
+
+    position: absolute;
+    flex-direction: column;
+    gap: 15px;
+  }
+  .bg {
+    background: #159957;
+    background: -webkit-linear-gradient(
+      to right,
+      #155799,
+      #159957
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #155799, #159957);
+  }
+</style>
---
+++ src/lib/QuoteCard.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let quote, author;
+</script>
+
+<main>
+  <div class="card border border-primary">
+    <div class="card-body p-5">
+      <h3 class="card-title lead text-center">
+        <i>{quote || "Loading..."}</i>
+      </h3>
+      <p class="mb-0 mt-3 blockquote text-center">
+        <mark>{author || "Loading..."}</mark>
+      </p>
+    </div>
+  </div>
+</main>
+
+<style>
+  .card {
+    margin-right: 50px;
+    margin-left: 50px;
+    cursor: pointer;
+    transition: 0.5s ease-in-out;
+  }
+  .card:hover {
+    transform: scale(1.05);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.12), 0 4px 8px rgba(0, 0, 0, 0.06);
+  }
+  h3 {
+    font-size: 25pt;
+  }
+  p {
+    font-weight: 600;
+  }
+</style>
